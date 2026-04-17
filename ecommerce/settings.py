ee9497ff4b52b4f1f4a185178550aeb8acd019ae
Michał Dzieliński <dzielinski.michal@gmail.com>
2026-04-17 07:04:58 +0200

    Another minor changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 #region BASE SETTINGS & DEPLOYMENT
 load_dotenv()
@@ -70,11 +71,12 @@ CRISPY_TEMPLATE_PACK='bootstrap5'
 #endregion
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+      "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True)
     }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
